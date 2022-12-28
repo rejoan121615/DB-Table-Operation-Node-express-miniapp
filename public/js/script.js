7c4c0af90f2346a30b8a2f8705e7961b457edbe9
Mohd Rejoan <wearecrx@gmail.com>
2022-12-28 11:51:04 +0600

    shared deatils insert form created

--- public/js/script.js
+++ public/js/script.js
@@ -17,17 +17,27 @@ if (alertWrap) {
 const insertTweakBtn = document.querySelector("#insertFormTweak");
 let activeDc = true; // dc or sh
 const insertBtns = ["Insert Visys Shared Details", "Insert Visys Dc Details"];
+const dcDetails = document.querySelector("#dcDetails");
+const sharedDetails = document.querySelector("#sharedDetails");
+
+function classUpdate(tag, a, b) {
+    tag.classList.remove(b);
+    tag.classList.add(a);
+    return tag;
+}
 
 insertTweakBtn.onclick = function () {
     if (activeDc) {
         insertTweakBtn.innerHTML = insertBtns[1];
-        insertTweakBtn.classList.remove("btn-info");
-        insertTweakBtn.classList.add("btn-warning");
+        classUpdate(insertTweakBtn, "btn-warning", "btn-info");
+        classUpdate(sharedDetails, "d-block", "d-none");
+        classUpdate(dcDetails, "d-none", "d-block");
         activeDc = false;
     } else {
         insertTweakBtn.innerHTML = insertBtns[0];
-        insertTweakBtn.classList.remove("btn-warning");
-        insertTweakBtn.classList.add("btn-info");
+        classUpdate(insertTweakBtn, "btn-info", "btn-warning");
+        classUpdate(dcDetails, "d-block", "d-none");
+        classUpdate(sharedDetails, "d-none", "d-block");
         activeDc = true;
     }
 };
