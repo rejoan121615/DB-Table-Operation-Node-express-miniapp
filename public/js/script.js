231f7f173c2c43aae5d7f7cacc70c1910db38de7
Mohd Rejoan <wearecrx@gmail.com>
2023-01-03 21:52:04 +0600

    navbar active item added

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
 console.clear();
 
+// ---------------- table control -------------------
 // all control btn
 const ctrlBtn = Array.from(document.querySelectorAll("[data-btn-name]"));
 const formList = Array.from(document.querySelectorAll("[data-form-name]"));
+const cardList = document.querySelectorAll("[data-card-name]");
 const navLink = document.querySelectorAll(".nav-item .nav-link");
 
 function HiddeAll(list) {
@@ -20,11 +22,22 @@ function UpdateFormBtn(condition) {
     const acBtn = ctrlBtn.filter((btn) => {
         return btn.getAttribute("data-btn-name") === condition;
     });
-    console.log('active btn', acBtn);
-    console.log('active form', acForm);
-    if (acBtn.length|| acForm.length) {
+    if (acBtn.length || acForm.length) {
         acBtn[0].classList.add("active");
         acForm[0]?.classList.add("active");
+    }
+    // update card list
+    if (cardList.length) {
+        const cards = Array.from(cardList);
+        if (condition === "all") {
+            cards.map((card) => (card.style.display = "block"));
+        } else {
+            const sliceCard = cards.filter((card) => {
+                card.style.display = "none";
+                return card.getAttribute("data-card-name") === condition;
+            });
+            sliceCard.map((card) => (card.style.display = "block"));
+        }
     }
 }
 
@@ -42,18 +55,18 @@ ctrlBtn.map((btn) => {
                 UpdateFormBtn(clickedAttr);
                 break;
             default:
-                console.log(clickedAttr);
-                console.log(ctrlBtn)
-                console.log('clicked')
                 UpdateFormBtn(clickedAttr);
                 break;
         }
     };
 });
 
-// Array.from(navLink).map((item) => {
-//     item.onclick = function (e) {
-//         e.preventDefault();
-//     };
-// });
-console.log('initialize')
+// navbar active creator
+const allNav = document.querySelectorAll(".nav .nav-item");
+Array.from(allNav).map((item) => {
+    if (item.querySelector("a").href === window.location.href) {
+        item.classList.add("active");
+    } else {
+        item.classList.remove("active");
+    }
+});
