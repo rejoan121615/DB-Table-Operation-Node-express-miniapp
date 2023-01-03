786f8aeeefafc83576ce09755d29c2ba24bf906f
Mohd Rejoan <wearecrx@gmail.com>
2023-01-03 07:05:38 +0600

    form condition added

--- public/js/script.js
+++ public/js/script.js
@@ -1,68 +1,46 @@
-const alertCross = document.querySelector("#cross-btn");
-const alertWrap = document.querySelector(".alert-wrap");
+console.clear();
 
-// timer
-if (alertWrap) {
-    const alertTimer = setTimeout(() => {
-        alertWrap.remove();
-    }, 3000);
+// all control btn
+const ctrlBtn = Array.from(document.querySelectorAll("[data-btn-name]"));
+const formList = Array.from(document.querySelectorAll("[data-form-name]"));
 
-    alertCross.onclick = function () {
-        alertWrap.remove();
-        clearInterval(alertTimer);
-    };
-}
+console.log(formList);
 
-// insert form tweak btn
-// const insertTweakBtn = document.querySelector("#insertFormTweak");
-// let activeDc = true; // dc or sh
-// const insertBtns = ["Insert Visys Shared Details", "Insert Visys Dc Details"];
-
-function classUpdate(tag, add, remove) {
-    tag.classList.remove(remove);
-    tag.classList.add(add);
-    return tag;
-}
-
-// insertTweakBtn.onclick = function () {
-//     if (activeDc) {
-//         insertTweakBtn.innerHTML = insertBtns[1];
-//         classUpdate(insertTweakBtn, "btn-warning", "btn-info");
-//         classUpdate(sharedDetails, "d-block", "d-none");
-//         classUpdate(dcDetails, "d-none", "d-block");
-//         activeDc = false;
-//     } else {
-//         insertTweakBtn.innerHTML = insertBtns[0];
-//         classUpdate(insertTweakBtn, "btn-info", "btn-warning");
-//         classUpdate(dcDetails, "d-block", "d-none");
-//         classUpdate(sharedDetails, "d-none", "d-block");
-//         activeDc = true;
-//     }
-// };
-
-const selectBtn = document.querySelector("#table_select_btn");
-// forms
-const detailsForm = document.querySelector("#details");
-const dcDetailsForm = document.querySelector("#dcDetails");
-const sharedDetailsForm = document.querySelector("#sharedDetails");
-const insertFormList = [detailsForm, dcDetailsForm, sharedDetailsForm];
-
-selectBtn.addEventListener("change", (e) => {
-    console.log(e.target)
-    // hide all form
-    insertFormList.map((item) => {
-        item ? classUpdate(item, "d-none", "d-block") : null;
+function HiddeAll(list) {
+    list.forEach((item) => {
+        item.classList.remove("active");
     });
-    // show spacific form
-    switch (e.target.value) {
-        case "details":
-            classUpdate(detailsForm, "d-block", "d-none");
-            break;
-        case "dc_details":
-            classUpdate(dcDetailsForm, "d-block", "d-none");
-            break;
-        case "shared_details":
-            classUpdate(sharedDetailsForm, "d-block", "d-none");
-            break;
-    }
+}
+
+function UpdateFormBtn(condition) {
+    HiddeAll(ctrlBtn);
+    HiddeAll(formList);
+    const acForm = formList.filter((form) => {
+        return form.getAttribute("data-form-name") === condition;
+    });
+    const acBtn = ctrlBtn.filter((btn) => {
+        return btn.getAttribute("data-btn-name") === condition;
+    });
+    acBtn[0].classList.add("active");
+    acForm[0].classList.add("active");
+}
+
+ctrlBtn.map((btn, index, btns) => {
+    btn.onclick = function () {
+        const clickedAttr = btn.getAttribute("data-btn-name");
+        switch (clickedAttr) {
+            case "dc":
+                UpdateFormBtn(clickedAttr);
+                break;
+            case "shared":
+                UpdateFormBtn(clickedAttr);
+                break;
+            default:
+                UpdateFormBtn(clickedAttr);
+                break;
+        }
+    };
 });
+
+// active form 
+UpdateFormBtn('details');
