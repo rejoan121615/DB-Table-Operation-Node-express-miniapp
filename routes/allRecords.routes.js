639d318342b88216a9d23a5857c89f65022adca4
Mohd Rejoan <wearecrx@gmail.com>
2023-01-09 03:39:39 +0600

    all record list added

--- routes/allRecords.routes.js
+++ routes/allRecords.routes.js
@@ -1,30 +1,12 @@
 const { Router } = require("express");
 const DcDetailsModel = require("../model/DcModel");
+const { allRecordGet } = require("../controllers/AllRecords");
 const router = Router();
 
 // delete data
 router.get("/", (req, res, next) => {
     res.redirect('reports/all')
 });
-router.get("/all", (req, res, next) => {
-    res.render("Record-list", {
-        type: "all",
-    });
-});
-router.get("/dc", (req, res, next) => {
-    res.render("Record-list", {
-        type: 'dc'
-    });
-});
-router.get("/details", (req, res, next) => {
-    res.render("Record-list", {
-        type: "details",
-    });
-});
-router.get("/shared", (req, res, next) => {
-    res.render("Record-list", {
-        type: "shared",
-    });
-});
+router.get("/:url", allRecordGet);
 
 module.exports = router;
